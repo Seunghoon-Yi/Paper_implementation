16fb078c1cae96850736fae12ead1f2428db75d8
Seunghoon-Yi <jaguar6182@snu.ac.kr>
2023-10-06 00:51:18 +0900

    Added EffNetV2

---
+++ CNNs/EffNetV2/EffNetV2/__init__.py
@@ -0,0 +1,6 @@
+import pretrained_weight_loader
+import configs
+import efficientnet_v2
+from .pretrained_weight_loader import load_from_zoo
+from .configs import get_efficientnet_v2_structure
+from .efficientnet_v2 import get_efficientnet_v2
